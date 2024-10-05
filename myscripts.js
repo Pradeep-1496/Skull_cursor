7f4e988019d047b4cb3e8fff68eb41ec4e73f517
pradeep-1496 <d23dit103@charusat.edu.in>
2024-10-05 13:00:45 +0530

    commit

---
+++ myscripts.js
@@ -0,0 +1,10 @@
+function save() {
+  let name = document.getElementById("name").value;
+  let num = document.getElementById("num").value;
+  let pass = document.getElementById("pass").value;
+  
+  document.write("Your Name is " + name + "<br>");
+  document.write("Your number is " + num + "<br>");
+  document.write("Your password is " + pass + "<br>");
+  
+}
